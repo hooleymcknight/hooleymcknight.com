87fffc0360a4c9a7c0843586597468abfdd73663
Holly Phillips <hmphillips93@gmail.com>
2022-11-23 02:23:12 -0800

    added base functionality for blog pages

---
+++ hooleymcknight-reacts/components/blog/blogHome.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import postList from './postList.json'
+
+const BlogHome = (props) => {
+  return (
+    <div className={props.className}>
+      blog home
+    </div>
+  )
+}
+
+export default BlogHome
---
+++ hooleymcknight-reacts/components/blog/blogPost.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import postList from './postList.json'
+
+const BlogPostTile = (props) => {
+  return (
+    <div className={props.className}>
+      
+    </div>
+  )
+}
+
+export default BlogPostTile
